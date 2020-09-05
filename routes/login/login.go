fd88e706b1e29debbcb9244d8f7e90058f6927ff
Takuma KATANOSAKA <bumpbumpbump.v@gmail.com>
2020-09-05 22:54:44 +0900

    ログ追加

--- routes/login/login.go
+++ routes/login/login.go
@@ -3,6 +3,7 @@ package login
 import (
 	"crypto/rand"
 	"encoding/base64"
+	"log"
 	"net/http"
 
 	"app"
@@ -10,11 +11,14 @@ import (
 )
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	log.Print("Open /login")
+
 	// Generate random state
 	b := make([]byte, 32)
 	_, err := rand.Read(b)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Fatal("Random Read Error")
 		return
 	}
 	state := base64.StdEncoding.EncodeToString(b)
@@ -22,18 +26,21 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	session, err := app.Store.Get(r, "auth-session")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Fatal("Session Get Error")
 		return
 	}
 	session.Values["state"] = state
 	err = session.Save(r, w)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Fatal("Session Save Error")
 		return
 	}
 
 	authenticator, err := auth.NewAuthenticator()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Fatal("Generate Authenticator Error")
 		return
 	}
 
