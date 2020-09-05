bbee5873ba734ebeaa70171298a0b2600be969c6
Takuma KATANOSAKA <bumpbumpbump.v@gmail.com>
2020-09-05 23:25:27 +0900

    コメント追加

--- routes/login/login.go
+++ routes/login/login.go
@@ -24,6 +24,7 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	state := base64.StdEncoding.EncodeToString(b)
 	log.Printf("Generated random state: %s", state)
 
+	// Create New Session
 	session, err := app.Store.Get(r, "auth-session")
 	if err != nil {
 		log.Fatal("Session Get Error")
@@ -39,6 +40,7 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	log.Print("Got Session")
 
+	// Create New Auth0 Authenticator
 	authenticator, err := auth.NewAuthenticator()
 	if err != nil {
 		log.Fatal("Generate Authenticator Error")
@@ -47,6 +49,7 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	log.Print("Got Authenticator")
 
+	// Redirect Lock Screen
 	log.Printf("Redirect %s", authenticator.Config.AuthCodeURL(state))
 	http.Redirect(w, r, authenticator.Config.AuthCodeURL(state), http.StatusTemporaryRedirect)
 }
